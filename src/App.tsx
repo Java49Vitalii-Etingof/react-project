5b73c8442dbb20ee7e32c01291caf21463861d28
Vitalii <vetingof@gmail.com>
2023-02-04 18:36:31 +0200

    Initial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
-import { Life } from './components/Life';
-import lifeConfig from './config/life-game.json'
+
 import './App.css'
 function App() {
   
 
-  return <div style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
-    <Life dimensions={lifeConfig.dimensions} ticInterval={lifeConfig.ticInterval}></Life>
-  </div>
+  return <div></div>
 
 }
 
